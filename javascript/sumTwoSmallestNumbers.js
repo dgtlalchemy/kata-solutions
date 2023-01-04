578d76b2147b31ce73fdd6c485dcebc25bf939c1
E Sperry <dgtlalchemy@gmail.com>
2023-01-04 15:43:12 -0800

    Update sumTwoSmallestNumbers.js

--- javascript/sumTwoSmallestNumbers.js
+++ javascript/sumTwoSmallestNumbers.js
@@ -1,3 +1,6 @@
+// Return the sum of the two smallest numbers in the array
+// Array passed in will be > 4 length of positive numbers
+
 function sumTwoSmallestNumbers(numbers) {  
   const n = numbers.sort((a, b) => a - b);
   return n[0] + n[1];
